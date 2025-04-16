37f2b42ed9651b79c35f3f5cf97f90053519776e
AnnuNain <145455659+AnnuNain@users.noreply.github.com>
2025-04-16 23:32:45 +0530

    Create LoginServlet2.java

---
+++ LoginServlet2.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/LoginServlet2")
+public class LoginServlet2 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html><head><title>Login Page</title></head><body>");
+        out.println("<h2>Login (Servlet Version)</h2>");
+        out.println("<form method='post' action='LoginServlet2'>");
+        out.println("Username: <input type='text' name='username' required><br><br>");
+        out.println("Password: <input type='password' name='password' required><br><br>");
+        out.println("<input type='submit' value='Login'>");
+        out.println("</form>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html><head><title>Login Result</title></head><body>");
+
+        if ("admin".equals(username) && "password123".equals(password)) {
+            out.println("<h2>Welcome, " + username + "!</h2>");
+        } else {
+            out.println("<h2>Invalid login. Try again.</h2>");
+            out.println("<a href='LoginServlet2'>Back to login</a>");
+        }
+
+        out.println("</body></html>");
+    }
+}
